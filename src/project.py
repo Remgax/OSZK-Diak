684e1bcca20426332e929d4910b18d0e0f2c334d
vigh Máté <vigh.mate@benkoiskola.hu>
2023-08-07 13:26:49 +0200

    Rekord feldolgozás Fixes #2

--- src/project.py
+++ src/project.py
@@ -1,21 +1,46 @@
+def counter():
 
-with open('source/output.txt', "r", encoding= "utf-8") as file:
+    with open('source/output.txt', "r", encoding= "utf-8") as file:
     
-    lines = file.readlines()
+        text = file.read()
 
-    count = 0
+        records = text.count("\n001")
 
-    for i in lines:
-        count += 1
+        print("Rekord azonosítók:", records)
 
-    print("Sorok száma:",count)
+        record_heads = text.count("\n000")
+        
+        print("Rekord fejek:", record_heads)
+
+        location = text.count("\n850")
+        
+        print("Lelőhej:",location)
+
+        storage_label = text.count("852$m")
+
+        print("Raktári jelzet:", storage_label)
+
+def label():
+    with open('source/output.txt', "r", encoding= "utf-8") as file:
+        lines = file.readlines()
+
+        labels = []
+
+        for i in lines:
+            if i.count("001") > 0 and i.count(" 001") == 0 and i.count(".001") == 0:
+                i = int(i.replace("001 ", ""))
+                labels.append(i)
+
+        labels.sort()
+        print("Jelzetek:", labels)
+
+counter()
+
+label()
 
 
-with open('source/output.txt', "r", encoding= "utf-8") as file:
-   
-    text = file.read()
-    records = text.count("\n001")
 
-    print("Rekordok:", records)
+
+
 
 
