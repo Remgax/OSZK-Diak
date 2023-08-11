7d4ed9539cb84964993754702ea2294c2f38f67d
vigh Máté <vigh.mate@benkoiskola.hu>
2023-08-11 09:59:24 +0200

    Menü rendszer Fixes #12

--- src/project.py
+++ src/project.py
@@ -1,63 +1,12 @@
-
+# 1 ----------------------------------------------------------------
 with open('source/output.txt', "r", encoding= "utf-8") as file:
-    
-    lines = file.readlines()
-
-    count = 0
-
-    for i in lines:
-        count += 1
-
-    print("Sorok száma:",count)
-
-
-with open('source/output.txt', "r", encoding= "utf-8") as file:
-   
+  
     text = file.read()
     records = text.count("\n001")
 
-    print("Rekordok:", records)
+    print("Rekordok száma:", records)
 
-#----------------------------------------------------------------
-
-def counter():
-
-    with open('source/output.txt', "r", encoding= "utf-8") as file:
-    
-        text = file.read()
-
-        records = 0
-        
-        for i in text.split('\n'):
-            if i.startswith('001'):
-                records += 1 
-
-        print("Rekord azonosítók:", records)
-
-        record_heads = 0
-
-        for i in text.split('\n'):
-            if i.startswith('000'):
-                record_heads += 1
-         
-        print("Rekord fejek:", record_heads)
-
-        location = 0
-
-        for i in text.split("\n"):
-            if i.startswith('850'):
-                location += 1
-
-        
-        print("Lelőhely:",location)
-
-        storage_label = 0
-
-        for i in text.split("\n"):
-            if i.startswith('852'):
-                storage_label += 1
-
-        print("Raktári jelzet:", storage_label)
+# 2 ----------------------------------------------------------------
 
 def label():
     with open('source/output.txt', "r", encoding= "utf-8") as file:
@@ -77,55 +26,14 @@ def label():
         for i in labels:
             print(f"{i},")
 
-counter()
-
 label()
 
-#----------------------------------------------------------------
+# 3 ----------------------------------------------------------------
 
 import time
 
 start_time = time.time()
 
-def counter():
-
-    with open('source/output.txt', "r", encoding= "utf-8") as file:
-    
-        text = file.read()
-
-        records = 0
-        
-        for i in text.split('\n'):
-            if i.startswith('001'):
-                records += 1 
-
-        print("Rekord azonosítók:", records)
-
-        record_heads = 0
-
-        for i in text.split('\n'):
-            if i.startswith('000'):
-                record_heads += 1
-         
-        print("Rekord fejek:", record_heads)
-
-        location = 0
-
-        for i in text.split("\n"):
-            if i.startswith('850'):
-                location += 1
-
-        
-        print("Lelőhely:",location)
-
-        storage_label = 0
-
-        for i in text.split("\n"):
-            if i.startswith('852'):
-                storage_label += 1
-
-        print("Raktári jelzet:", storage_label)
-
 def label():
     with open('source/output.txt', "r", encoding= "utf-8") as file:
         lines = file.readlines()
@@ -139,15 +47,47 @@ def label():
 
         labels.sort()
 
-        print("\nAzonosítók:")
-
-        for i in labels:
-            print(f"{i},")
-
-counter()
-
 label()
 
-print(f" {(time.time() - start_time)} seconds")
+# 4 ----------------------------------------------------------------
 
-#----------------------------------------------------------------
+
+
+# 5 ----------------------------------------------------------------
+
+def date():
+    with open('source/output.txt', "r", encoding= "utf-8") as file:
+
+        text = file.read()
+
+        date_sorted = []
+
+        for i in text.split('\n'):
+            if i.startswith('005'):
+                i = i.replace('005', '').replace('.0', '')
+                date_sorted.append(i)
+                date_sorted.sort()
+
+                year = i[1:5]
+                month = i[5:7]
+                day = i[7:9]
+                hour = i[9:11]
+                minute = i[11:13]
+                second = i[13:15]
+
+            
+        print("\nIdőrendben:\n")
+        for i in date_sorted:
+            
+            year = i[1:5]
+            month = i[5:7]
+            day = i[7:9]
+            hour = i[9:11]
+            minute = i[11:13]
+            second = i[13:15]
+
+            print(f"{year}. {month}. {day}. {hour}:{minute}:{second}")
+
+date()
+
+margin = 29*"-"
