62edb85a81dbbb3a0ef29a3c9c190117c023b912
vigh Máté <vigh.mate@benkoiskola.hu>
2023-08-11 09:33:09 +0200

    Menü rendszer Fixes #12

--- src/project.py
+++ src/project.py
@@ -18,4 +18,67 @@ with open('source/output.txt', "r", encoding= "utf-8") as file:
 
     print("Rekordok:", records)
 
+#----------------------------------------------------------------
 
+def counter():
+
+    with open('source/output.txt', "r", encoding= "utf-8") as file:
+    
+        text = file.read()
+
+        records = 0
+        
+        for i in text.split('\n'):
+            if i.startswith('001'):
+                records += 1 
+
+        print("Rekord azonosítók:", records)
+
+        record_heads = 0
+
+        for i in text.split('\n'):
+            if i.startswith('000'):
+                record_heads += 1
+         
+        print("Rekord fejek:", record_heads)
+
+        location = 0
+
+        for i in text.split("\n"):
+            if i.startswith('850'):
+                location += 1
+
+        
+        print("Lelőhely:",location)
+
+        storage_label = 0
+
+        for i in text.split("\n"):
+            if i.startswith('852'):
+                storage_label += 1
+
+        print("Raktári jelzet:", storage_label)
+
+def label():
+    with open('source/output.txt', "r", encoding= "utf-8") as file:
+        lines = file.readlines()
+
+        labels = []
+
+        for i in lines:
+            if i.count("001") > 0 and i.count(" 001") == 0 and i.count(".001") == 0:
+                i = int(i.replace("001 ", ""))
+                labels.append(i)
+
+        labels.sort()
+
+        print("\nAzonosítók:")
+
+        for i in labels:
+            print(f"{i},")
+
+counter()
+
+label()
+
+#----------------------------------------------------------------
